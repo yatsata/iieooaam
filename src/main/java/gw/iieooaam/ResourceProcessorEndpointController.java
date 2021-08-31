3611953b59da8ee359ea5f87d1cba1b33eb92879
yatsata <36297884+yatsata@users.noreply.github.com>
2021-08-31 14:36:33 +0300

    Add greatest endpoint following the best practices

---
+++ src/main/java/gw/iieooaam/ResourceProcessorEndpointController.java
@@ -0,0 +1,20 @@
+package gw.iieooaam;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+
+@Path("/find")
+public class ResourceProcessorEndpointController {
+
+    //Patch is the most logical option here
+    @PATCH
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/find-is-it-even-or-odd")
+    public String findItOut(@QueryParam(value="username") String username,
+                            @QueryParam(value="password") String password) {
+        // just to be sure they are correct print the credentials
+        return "{ \n \"the answer iz\": \"maybe\", \n \"credentials\": \"" + password + username+"\" \n}";
+    }
+}
