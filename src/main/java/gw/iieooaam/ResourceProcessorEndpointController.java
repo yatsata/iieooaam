0cad7ef770451b7bd246a58e97a942c7fe3f6f18
yatsata <36297884+yatsata@users.noreply.github.com>
2021-09-01 20:47:44 +0300

    Add the least important thing

--- src/main/java/gw/iieooaam/ResourceProcessorEndpointController.java
+++ src/main/java/gw/iieooaam/ResourceProcessorEndpointController.java
@@ -13,7 +13,8 @@ public class ResourceProcessorEndpointController {
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/find-is-it-even-or-odd")
     public String findItOut(@QueryParam(value="username") String username,
-                            @QueryParam(value="password") String password) {
+                            @QueryParam(value="password") String password,
+                            @QueryParam(value="theChecked") String theChecked) {
         // just to be sure they are correct print the credentials
         return "{ \n \"the answer iz\": \"maybe\", \n \"credentials\": \"" + password + username+"\" \n}";
     }
