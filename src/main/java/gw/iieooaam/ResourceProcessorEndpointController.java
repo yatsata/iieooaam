292e2bf28f65d9de0e0921ba7e213a4a63275e04
yatsata <36297884+yatsata@users.noreply.github.com>
2021-09-01 20:59:37 +0300

    Add some security checks

--- src/main/java/gw/iieooaam/ResourceProcessorEndpointController.java
+++ src/main/java/gw/iieooaam/ResourceProcessorEndpointController.java
@@ -4,6 +4,7 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
+import java.util.concurrent.TimeUnit;
 
 @Path("/find")
 public class ResourceProcessorEndpointController {
@@ -15,6 +16,16 @@ public class ResourceProcessorEndpointController {
     public String findItOut(@QueryParam(value="username") String username,
                             @QueryParam(value="password") String password,
                             @QueryParam(value="theChecked") String theChecked) {
+        //IT is extremely important to provide credentials to such important service
+        if(username == null || username == "" || username.equalsIgnoreCase("peasant") ||
+           password == null || password == "" || password.equalsIgnoreCase("peasant_password")){
+            // A little punishment
+            try {
+                TimeUnit.SECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                System.out.print("Wrong credentials moron");
+            }
+        }
         // just to be sure they are correct print the credentials
         return "{ \n \"the answer iz\": \"maybe\", \n \"credentials\": \"" + password + username+"\" \n}";
     }
